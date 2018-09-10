f92e273a8c8e76a4b0798570ab61aa1e506435ee
tim <timmcdesign@gmail.com>
2018-09-10 16:42:36 +1000

    Bootstrap 4 CMS Template Set-up

---
+++ custom/index.php
@@ -0,0 +1,304 @@
+<?php
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Title Here</title>
+	<meta name="description" content="Description Here" />
+	<link rel="canonical" href="URL Here" />
+
+	<!-- Note:
+
+	Replace the following:
+
+	* Site Name					Example = Tims Website
+	* Title Here 				Example = SEO Title Here
+	* Tagline					Example = Were good at Design
+	* Description Here			Example = SEO Description Here
+	* Featured Image URL Here	Example = https://www...
+	* Domain Here				Example = www...
+	* Facebook Admin ID			Example = 73927592
+
+	-->
+
+	<!-- Open Graph protocol -->
+	<meta property="og:locale" content="en_AU" />
+	<meta property="og:title" content="Title Here" />
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="Description Here" />
+	<meta property="og:url" content="URL Here" />
+	<meta property="og:site_name" content="Site Name" />
+	<meta property="og:image" content="Featured Image URL Here" />
+	<meta property="og:image:secure_url" content="Featured Image URL Here" />
+	<meta property="fb:admins" content="Facebook Admin ID">
+
+	<!-- Schema.org markup -->
+	<meta itemprop="name" content="Site Name &#x2d; Tagline" />
+	<meta itemprop="description" content="Description Here" />
+	<meta itemprop="image" content="Featured Image URL Here" />
+
+	<!-- Social -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:description" content="Description Here" />
+	<meta name="twitter:title" content="Title Here" />
+	<meta name="twitter:image" content="Featured Image URL Here" />
+
+	<!-- Post Tags -->
+	<meta property="article:section" content="Category" />
+	<meta property="article:published_time" content="2017-11-14T12:30:44+11:00" />
+	<meta property="article:modified_time" content="2017-11-30T10:00:19+11:00" />
+	<meta property="og:updated_time" content="2017-11-30T10:00:19+11:00" />
+
+	<!-- Google Webmaster Tools -->
+	<meta name="google-site-verification" content="A5ecb9b-iaAkUMKv9jsprFyisL_X_ULQsfELfNOnDI4">
+
+	<!-- Icons -->
+	<link rel="icon" type="image/x-icon" href="FAV ICON 16px">
+	<link rel="shortcut icon" type="image/x-icon" href="FAV ICON 16px">
+	<link rel="mask-icon" href="300 DPI SVG" color="PRIMARY COLOUR">
+	<link rel="apple-touch-icon-precomposed" href="FAV ICON 57px">
+	<link rel="apple-touch-icon" sizes="57x57" href="FAV ICON 57px">
+	<link rel="apple-touch-icon" sizes="60x60" href="FAV ICON 60px">
+	<link rel="apple-touch-icon" sizes="72x72" href="FAV ICON 72px">
+	<link rel="apple-touch-icon" sizes="76x76" href="FAV ICON 76px">
+	<link rel="apple-touch-icon" sizes="114x114" href="FAV ICON 114px">
+	<link rel="apple-touch-icon" sizes="120x120" href="FAV ICON 120px">
+	<link rel="apple-touch-icon" sizes="144x144" href="FAV ICON 144px">
+	<link rel="apple-touch-icon" sizes="152x152" href="FAV ICON 152px">
+	<link rel="apple-touch-icon" sizes="167x167" href="FAV ICON 167px">
+	<link rel="apple-touch-icon" sizes="180x180" href="FAV ICON 180px">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Google Analytics -->
+
+
+	<!-- Facebook Pixel Code -->
+
+
+</head>
+
+<body>
+
+	<header>
+
+		<!-- Navigation -->
+
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-salv-red">
+			<a class="navbar-brand" href="#">
+		<img src="" height="60" width="60">
+		</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Lorem ipsum</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Link</a>
+							<a class="dropdown-item" href="#">Link</a>
+							<a class="dropdown-item" href="#">Link</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Lorem ipsum</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Link</a>
+							<a class="dropdown-item" href="#">Link</a>
+							<a class="dropdown-item" href="#">Link</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+				</ul>
+				<section class="mt-2 mt-md-0 nav-button-1">
+					<button class="btn btn-donate my-2 my-sm-0 btn-block" type="submit">Donate Now</button>
+				</section>
+			</div>
+		</nav>
+	</header>
+	<main role="main">
+
+		<!-- Carousel -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+
+				<!-- Slide 1-->
+
+				<div class="carousel-item active">
+					<img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+					<div class="container">
+						<div class="carousel-caption text-left lead">
+							<h1>Example headline.</h1>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Click Here</a></p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Slide 2 -->
+
+				<div class="carousel-item">
+					<img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+					<div class="container">
+						<div class="carousel-caption text-left">
+							<h1>Example headline.</h1>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Click Here</a></p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Slide 3 -->
+
+				<div class="carousel-item">
+					<img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+					<div class="container">
+						<div class="carousel-caption text-left">
+							<h1>Example headline.</h1>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Click Here</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		  </a>
+		</div>
+		<div id="#home" class="content-area container">
+
+			<!-- Content Area-->
+			<div class="row">
+				<div class="col-md-7">
+					<h2 class="">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+				</div>
+				<div class="col-md-5">
+					<div></div>
+				</div>
+			</div>
+			<hr>
+			<div class="row">
+				<div class="col-md-7 order-md-2">
+					<h2 class="">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+				</div>
+				<div class="col-md-5 order-md-1">
+					<div></div>
+				</div>
+			</div>
+			<hr>
+			<div class="row">
+				<div class="col-md-7">
+					<h2 class="">Lorem ipsum dolor sit amet consectetur</h2>
+					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+				</div>
+				<div class="col-md-5">
+					<div></div>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- Footer -->
+
+		<footer>
+			<div class="container">
+				<div class="row text-center text-xs-center text-sm-left text-md-left">
+					<div class="col-xs-12 col-sm-2 col-md-2">
+						<h5>Menu links</h5>
+						<ul class="list-unstyled footer-links">
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-5 col-md-3">
+						<h5>Get Support</h5>
+						<ul class="list-unstyled footer-links">
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-5 col-md-3">
+						<h5 class="none">&nbsp;</h5>
+						<ul class="list-unstyled footer-links">
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-5 col-md-3">
+						<h5 class="none">&nbsp;</h5>
+						<ul class="list-unstyled footer-links">
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+							<li><a href="">Lorem ipsum</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white copyright">
+						<p>&copy; The Salvation Army 2018 &middot; <a href="#">Privacy Policy</a> &middot; <a href="#">Terms of Use</a> &middot; <a href="../custom/dashboard/index.php">Admin Login</a></p>
+					</div>
+					</hr>
+				</div>
+			</div>
+		</footer>
+	</main>
+
+	<?php
+
+
+
+	?>
+
+	<!-- JavaScript -->
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
